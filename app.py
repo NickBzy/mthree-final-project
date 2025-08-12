53e596b2ff711bd86260504a9c440e79a502fa4c
NickBzy <97320296+NickBzy@users.noreply.github.com>
2025-08-12 18:33:42 -0400

    Add connection to mysql database

--- app.py
+++ app.py
@@ -1,43 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template
 import mysql.connector
-conn=mysql.connector.connect(host="localhost", user="root", password="sample", database="restaurant")
+import os
+
+# Change to environment variables later
+conn = mysql.connector.connect(
+    host=os.environ.get("DB_HOST", "3.139.96.127"),
+    port=int(os.environ.get("DB_PORT", 3306)),
+    user=os.environ.get("DB_USER", "flaskuser"),
+    password=os.environ.get("DB_PASS", "flaskpass"),
+    database=os.environ.get("DB_NAME", "restaurant"),
+)
 cursor=conn.cursor()
 
 app=Flask("restaurant")
 
 @app.route('/')
 def index():
-    msg = '''
-<body>
-    <header>
-        <h1>Welcome to Our Restaurant</h1>
-    </header>
-    
-    <nav>
-        <h2>Main Navigation</h2>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/menu/">Menu</a></li>
-            <li><a href="/reservation/">Reservation</a></li>
-            <li><a href="/orders/">Orders</a></li>
-            <li><a href="/tables/">Tables</a></li>
-        </ul>
-    </nav>  
-    
-    <footer>
-        <p>© 2023 Our Restaurant. All rights reserved.</p>
-    </footer>
-</body>'''
-    return msg
+    return render_template('index.html')
 
 
 @app.route("/orders")
-
+def orders():
+    pass
 
 @app.route("/menu")
-
+def menu():
+    pass
 
 @app.route("/tables")
-
+def tables():
+    pass
 
 @app.route("/reservations")
+def reservations():
+    pass
+
+app.run("0.0.0.0", port=5000, debug=True)
